65c2e99d317634103ae88c02a995a0e8ec14e7da
CodeSandbox <support@codesandbox.io>
2024-08-31 07:05:01 +0000

    Sandbox snapshot commit

---
+++ .output/server/chunks/app/styles.mjs
@@ -0,0 +1,9 @@
+const interopDefault = r => r.default || r || [];
+const styles = {
+  "node_modules/.pnpm/nuxt@3.8.1_vite@4.5.0/node_modules/nuxt/dist/app/entry.js": () => import('./_nuxt/entry-styles.26cb16dd.mjs').then(interopDefault),
+  "node_modules/.pnpm/@nuxt+ui-templates@1.3.1/node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": () => import('./_nuxt/error-404-styles.d352c5f6.mjs').then(interopDefault),
+  "node_modules/.pnpm/@nuxt+ui-templates@1.3.1/node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": () => import('./_nuxt/error-500-styles.a47c06d2.mjs').then(interopDefault)
+};
+
+export { styles as default };
+//# sourceMappingURL=styles.mjs.map
