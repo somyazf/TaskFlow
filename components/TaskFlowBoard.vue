992ad5295aef1ccfa7fbbf029ec3c9979840f702
CodeSandbox <support@codesandbox.io>
2024-09-11 20:01:30 +0000

    Sandbox snapshot commit

--- components/TaskFlowBoard.vue
+++ components/TaskFlowBoard.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { nanoid } from 'nanoid';
-import type { Column } from '~/types';
+import type { Column, Task } from '~/types';
 import draggable from 'vuedraggable';
 
 const columns = ref<Column[]>([
@@ -50,14 +50,31 @@ const columns = ref<Column[]>([
             v-model="columns"
             group="columns"
             item-key="id"
+            handle=".drag-handle"
+            :animation="150"
             class="flex items-start overflow-x-auto gap-4"
         >
             <template #item="{ element: column }: { element: Column }">
                 <div class="column bg-gray-200 p-5 rounded min-w-[250px]">
                     <header class="font-bold mb-4">
+                        <DragHandle />
                         {{ column.title }}
                     </header>
-                    <TaskFlowTask v-for="task in column.tasks" :task="task" />
+                    <draggable
+                        v-model="column.tasks"
+                        group="tasks"
+                        handle=".drag-handle"
+                        :animation="150"
+                        item-key="id"
+                    >
+                    <template #item="{ element: task }: { element: Task }">
+                    <div>
+                        <TaskFlowTask
+                        :task="task"
+                        />
+                    </div>
+                    </template>
+                </draggable>
                     <footer>
                         <button> Add a card</button>
                     </footer>
--- components/TaskFlowTask.vue
+++ components/TaskFlowTask.vue
@@ -7,6 +7,7 @@ defineProps<{
 <template>
     <div :title="task.createdAt.toLocaleDateString()"
     class="task bg-white p-2 mb-2 rounded shadow-sm max-w-[250px] flex">
+        <DragHandle class="pr-2" />
         <span>{{ task.title }}</span>
     </div>
 </template>
